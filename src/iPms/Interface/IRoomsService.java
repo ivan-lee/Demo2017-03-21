d3cdefdc6068fa2810cfb070d0798898f2448fd1
chshi <chishi@beyondh.com>
2017-03-21 14:59:35 +0800

    Initial commit

---
+++ src/iPms/Interface/IRoomsService.java
@@ -0,0 +1,11 @@
+package iPms.Interface;
+
+/**
+ * Created by admin on 2017/3/21.
+ */
+public class IRoomsService {
+    public static void main(String[] args) {
+        System.out.println( "hello");
+
+    }
+}
